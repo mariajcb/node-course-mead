6cb3c11e6701c06e3463ef5151a1e201eee88ea7
Maria Bogomaz <bogomazm@gmail.com>
2020-03-09 10:39:40 -0600

    initial commit

---
+++ notes-app/notes.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const listNotes = () => {
+    const notes = loadNotes()
+    console.log(chalk.inverse('Your notes'))
+    return notes.forEach(note => console.log(note.title)) 
+}
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find(note => note.title === title)
+
+    if (note) {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.red.inverse('Note not found'))
+    }
+}
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = notes.find(note => note.title === title)
+
+    if (!duplicateNote) {
+        notes.push({
+            title,
+            body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added'));
+    } else {
+        console.log(chalk.red.inverse('Note title taken'));  
+    }
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(note => note.title !== title)
+
+    if(notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }
+    saveNotes(notesToKeep)
+}
+
+module.exports = {
+    addNote,
+    removeNote,
+    readNote,
+    listNotes
+}
---
+++ playground/1-json.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
+
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+
+user.name = 'Gunther'
+user.age = 54
+
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
